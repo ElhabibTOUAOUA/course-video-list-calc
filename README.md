92a87dc71b4c2310d058c769ba52776401c01912
Elhabib TOUAOUA <bibahhabib8@gmail.com>
2023-01-10 10:45:44 +0100

    This commit contains an enhanced by ChatGPT script of calculating and extracting courses vidoes.

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-This Project is a script that extract list of course materials associated with the duration of each video from a local course file.
-It also calculate the Duration of the course starting from each and every video on the course folder.
-The output of the script is a txt files, one contains course name + duration of the course.
-second txt file contains list of the course videos with duration of each.
+# VideoCourseListAndDuration
+
+A Python script that helps in calculating the duration of a course, which is provided in the form of videos and it lists the videos in a course as well.
+
+## Requirements
+
+-   openCV
+-   os
+-   datetime
+-   re
+-   tqdm
+
+## Functionality
+
+### `remove_non_video_files(files)`
+
+This function filters out the non-video files from the given list of files. It uses a regular expression to match the file names with the pattern of common video file extensions such as mp4, mkv, avi, flv, mov, wmv, vob, mpg, 3gp, and m4v.
+
+### `sort_list(unsorted_list)`
+
+This function sorts the given list of items in the format of 'element number' + 'split character' + 'element name'
+
+### `remove_non_numeric_elements(lst)`
+
+This function removes the non-numeric elements from the given list.
+
+### `course_duration(course_path, course_name)`
+
+This function calculates the duration of the course by traversing through the directories and files in the given course path and extracting the duration of each video using openCV's `VideoCapture` class. The duration is calculated by counting the total number of frames in the video and dividing it by the frame rate of the video. The calculated duration is written to a text file with the name format 'course_name_duration.txt' in the Output/Courses Duration folder.
+
+### `course_video_list(course_path, course_name)`
+
+This function lists the videos in a course by traversing through the directories in the given course path. It writes the list of videos to a text file with the name format 'course_name.txt' in the Output/Courses Video lists folder.
+
+## Usage
+
+```python
+course_path = 'path/to/course'
+course_name = 'example_course'
+course_duration(course_path, course_name)
+course_video_list(course_path, course_name)
+```
+
+## Output
+
+The script will generate two text files, one for duration and the other for videos list, in the Output folder.
+
+## Note
+
+-   The script assumes that the course is provided in the format of directories containing videos and the videos are named in the format of 'element number' + 'split character' + 'element name'
+-   The script assumes that the output folders ('Output/Courses Duration/' and 'Output/Courses Video lists/') already exist.
+-   It's recommended to run the script on a high-performance machine because of the heavy computation required for processing video frames.
+-   It may be necessary to adjust the regular expression in the remove_non_video_files function to match your specific naming conventions.
+-   It's a good practice to verify the output files to check the correctness of the calculated duration and video list.
+-   The script doesn't currently handle errors that may occur while reading the video files, such as file not found errors, and it may be necessary to add error handling to the script to handle such scenarios.
+-   It could be wise to check if the folder where the files are being written to are present, if not, you could make those folders on runtime for the code to work seamlessly.
---
+++ video_course_list_and_duration.py
@@ -0,0 +1,104 @@
+import os
+import cv2
+import datetime
+import re
+import tqdm
+
+def remove_non_video_files(files):
+	VIDEO_FILE_PATTERN = r'.*\.(mp4|mkv|avi|flv|mov|wmv|vob|mpg|3gp|m4v)$'
+	return [file for file in files if re.match(VIDEO_FILE_PATTERN, file)]
+
+def sort_list(unsorted_list):
+	# Split the list elements into a list of lists, where each inner list
+	# contains the element number, split character, and element name
+	lst = []
+	for item in unsorted_list:
+	# Find the split character using a regular expression
+		split_char = re.search(r'[^\d][\d]*(?=\D)', item).group()
+
+		# Split the element into the element number, split character, and element name
+		item_num, item_name = item.split(split_char, 1)
+		lst.append([int(item_num), item_num,split_char, item_name])
+		
+	# Sort the list by element number
+	lst.sort(key=lambda x: x[0])
+	# Build the sorted list by combining the element number, split character, and element name
+	sorted_list = [f"{item[1]}{item[2]}{item[3]}" for item in lst]
+	return sorted_list
+
+def remove_non_numeric_elements(lst):
+	return [item for item in lst if item[0].isdigit()]
+
+def course_duration(course_path, course_name):
+	with open(f"Output/Courses Duration/{course_name}_duration.txt", "w") as text_file:
+			seconds = 0
+			dirs = os.listdir(course_path)
+			dirs = remove_non_numeric_elements(dirs)
+			dirs = sort_list(dirs)
+			for dir in dirs:
+					current_dir = os.path.join(course_path, dir)
+					files = os.listdir(current_dir)
+					files = remove_non_numeric_elements(files)
+					files = remove_non_video_files(files)
+					files = sort_list(files)
+					# print(f"Sorted Files: {files}")
+					for file in tqdm.tqdm(files, desc='Calculating duration'):
+						video_path = os.path.join(current_dir, file)
+						try:
+								data = cv2.VideoCapture(video_path)
+								frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
+								fps = data.get(cv2.CAP_PROP_FPS)
+								if fps != 0:
+										seconds += round(frames / fps)
+								else:
+										print(f'\n{video_path} -- frames={frames} -- fps={fps}')
+						except Exception as e:
+								print(f'Error while processing {video_path}: {e}')
+			hours = int(seconds / 3600)
+			minutes = int(seconds / 60) % 60
+			text_file.write(f"Course duration = {hours}h {minutes}m")
+	print('Duration Calc finished with Success!!!')
+
+def course_video_list(course_path, course_name):
+	with open(f"Output/Courses Video lists/{course_name}.txt", "w") as text_file:
+			dirs = os.listdir(course_path)
+			dirs = remove_non_numeric_elements(dirs)
+			dirs = sort_list(dirs)
+			for dir in dirs:
+					# Write the current directory name to the file
+					text_file.write(f'------------------ {dir} ------------------\n')
+					current_dir = os.path.join(course_path, dir)
+					files = os.listdir(current_dir)
+					files = remove_non_numeric_elements(files)
+					files = remove_non_video_files(files)
+					files = sort_list(files)
+					for file in tqdm.tqdm(files, desc='Extracting video list'):
+							seconds = 0
+							video_path = os.path.join(current_dir, file)
+							data = cv2.VideoCapture(video_path)
+							frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
+							fps = data.get(cv2.CAP_PROP_FPS)
+							if fps != 0:
+									seconds = round(frames / fps)
+							else:
+									print(f'\n{video_path} -- frames={frames} -- fps={fps}')
+							video_time = datetime.timedelta(seconds=seconds)
+							formatted_time = '{:02d}:{:02d}'.format(video_time.seconds // 60, video_time.seconds % 60)
+							# Write the file name and duration to the file
+							text_file.write(f"{file.replace('.mp4', '')} | {formatted_time}\n")
+	print('Videos List Extraction finished with success')
+
+
+def course_duration_list(course_name):
+	base_path = os.path.join("E:\\","Courses", "Web Development",course_name)
+	course_name = base_path.split('\\')[-1]
+	course_duration(base_path, course_name)
+	course_video_list(base_path, course_name)
+
+if __name__ == '__main__':
+	courses_dir = os.path.join("E:\\","Courses", "Web Development")
+	courses = [course for course in os.listdir(courses_dir) if os.path.isdir(os.path.join(courses_dir, course))]
+	for course in courses:
+			print("\n=============================================")
+			print(f"Processing {course}")
+			course_duration_list(course)
